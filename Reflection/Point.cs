b192a07b7813c4019aedff7827f6b8e58a94adf1
Bali Vaibhav <bail.vaibhav@recreo.com.au>
2017-11-20 14:07:10 +1100

    Section 1 to 5 From Intro until Properties API demo

---
+++ Reflection/Point.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflection
+{
+    public class Point
+    {
+        public Point(int x, int y) { X = x; Y = y; }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public static double Distance(Point lhs, Point rhs)
+        {
+            return Math.Sqrt(
+                (Math.Pow((double)rhs.X - (double)lhs.X, 2)) +
+                (Math.Pow((double)rhs.Y - (double)lhs.Y, 2)));
+        }
+
+        public static Point ORIGIN { get { return new Point(0, 0); } }
+
+        public override string ToString()
+        { return String.Format("({0}, {1})", X, Y); }
+
+        public override bool Equals(object other)
+        {
+            if (Object.ReferenceEquals(this, other))
+                return true;
+
+            if (!(other is Point))
+                return false;
+
+            Point rhs = (Point)other;
+            return (X == rhs.X) && (Y == rhs.Y);
+        }
+
+        public static bool operator ==(Point lhs, Point rhs)
+        { return (lhs.X == rhs.X) && (lhs.Y == rhs.Y); }
+        public static bool operator !=(Point lhs, Point rhs)
+        { return !(lhs.X == rhs.X); }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Reflection;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basics();
+
+            var point = new Point(5,5);
+
+            var mirroredPoint = new Mirror(point.GetType());
+            mirroredPoint.DumpType();
+            mirroredPoint.DumpProperties();
+
+            var mirroredObject = new Mirror(typeof(System.Int32));
+            mirroredObject.DumpType();
+        }
+
+        private static Point Basics()
+        {
+            //Loading type using they type itself
+            var ptType = typeof(Point);
+            Console.WriteLine(ptType);
+
+            //Loading type using the object
+            var point = new Point(5, 5);
+            var ptTypeObj = point.GetType();
+            Console.WriteLine(ptTypeObj);
+
+            //Loading type using the static mehtod by string literal
+            var loadedType = System.Type.GetType("Reflection.Point");
+            Console.WriteLine(loadedType);
+
+            var ptAssem = ptType.Assembly;
+            Console.WriteLine(ptAssem.FullName);
+            Console.WriteLine(ptAssem.CodeBase);
+
+            //Assemblies are made up of modules and modules are in term host types
+            //Generally each Assembly as one Module 
+            foreach (var ptAssemModule in ptAssem.Modules)
+            {
+                Console.WriteLine(ptAssemModule);
+            }
+            return point;
+        }
+    }
+
+    //This class will act as Ildasm
+    public class Mirror
+    {
+        public Mirror(Type type)
+        {
+            MirroredType = type;
+        }
+
+        public Type MirroredType { get; set; }
+
+
+        public void DumpType()
+        {
+            Console.WriteLine($"{MirroredType.Name} loaded from {MirroredType.Assembly.FullName}");
+            Console.WriteLine($" Inherits from {MirroredType.BaseType.Name}");
+            Console.WriteLine($" implements:");
+            foreach (var @interface in MirroredType.GetInterfaces())
+            {
+                Console.WriteLine($"        {@interface.Name}");
+            }
+        }
+
+
+        public void DumpProperties()
+        {
+            Console.WriteLine("Properties:");
+            var flags = BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic;
+            foreach (var propertyInfo in MirroredType.GetProperties(flags))
+            {
+                Console.WriteLine($" {propertyInfo.Name} : {propertyInfo.PropertyType}");
+                Console.WriteLine($"   r: {propertyInfo.CanRead}, w:{propertyInfo.CanWrite}");
+            }
+        }
+    }
+
+    
+}
